3d8be53270e2318dd00236ce8766a534bf831bb4
Konlawat <Konlawateiei@gmail.com>
2020-10-13 22:53:34 +0700

    pondzapkw123

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+$(function(){
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+      apiKey: "AIzaSyD3CA8uNNLYKTiLda76DJyi0qXBWIJpljw",
+      authDomain: "cnema-c284d.firebaseapp.com",
+      databaseURL: "https://cnema-c284d.firebaseio.com",
+      projectId: "cnema-c284d",
+      storageBucket: "cnema-c284d.appspot.com",
+      messagingSenderId: "677283409519",
+      appId: "1:677283409519:web:7778c3d67f4ac1bfb8279a",
+      measurementId: "G-E9RPXMD7HD"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    var db = firebase.firestore();
+
+})
+    
