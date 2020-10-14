be8a5515d74c78dd106fca03aa3688c363932346
fhutzchx <fahut.ittiwat@gmail.com>
2020-10-15 01:54:33 +0700

    Natthinee Update google signin

--- js/index.js
+++ js/index.js
@@ -3,7 +3,14 @@ firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // User is signed in.
     var email = user.email;
-    console.log(`User with email ${email} signed in`);
+    displayName = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    console.log(displayName, email, photoUrl);
+
+    $("#username").text(email);
+    $("#displayname").text(displayName);
+    $("#photo").attr("src",photoUrl);
   } else {
     // User is signed out.
     window.location.href = "login.html";
