7c0c1d0a15a21ff89bcb2c17f6728c31b349f014
fhutzchx <fahut.ittiwat@gmail.com>
2020-10-18 05:38:08 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD3CA8uNNLYKTiLda76DJyi0qXBWIJpljw",
+  authDomain: "cnema-c284d.firebaseapp.com",
+  databaseURL: "https://cnema-c284d.firebaseio.com",
+  projectId: "cnema-c284d",
+  storageBucket: "cnema-c284d.appspot.com",
+  messagingSenderId: "677283409519",
+  appId: "1:677283409519:web:7778c3d67f4ac1bfb8279a",
+  measurementId: "G-E9RPXMD7HD"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
+
 // App logic.
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
