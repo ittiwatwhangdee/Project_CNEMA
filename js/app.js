037e416e95dd264db93803578533c14de263bdad
pondzapkw123 <konlawateiei@gmail.com>
2020-10-18 12:58:20 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -69,26 +69,99 @@ document.addEventListener('init', function (event) {
         var onsItem = document.createElement('ons-carousel-item');
         onsItem.innerHTML = item;
         page.querySelector('#carousel').appendChild(onsItem);
-        
+
       });
     });
   }
-  function MoviesDetail(id){
-    db.collection("movies").get().then(function(querySnapshot){
-      querySnapshot.forEach(function (doc) {
-        if(doc.data().title == id){
-          const detail =
+
+
+
+
+
+  function openMovieDetails(id) {
+    db.collection("movies").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        if (doc.data().title == id) {
+         /* const wallpaper =
+            `
+            <ons-icon icon="fa-play-circle"></ons-icon>
+            <img src="${doc.data().wellpaperURL}">
+          
+            `
+          $(".trailer").append(wallpaper)*/
+
+
+          const show_pic =
+            `
+            <img src="${doc.data().posterURL}"alt="" width="60%">
+         
           `
-          ${data().title}
-          `
-          $("#title").append(detail);
+          $("#show_pic").append(show_pic)
+
+          $("#movie_title").append(doc.data().title)
+          $("#movie_year").append(doc.data().year)
+          $(".movie_plot").append(doc.data().shortstory)
+          $(".movie_cast").append(doc.data().cast)
+          var s = doc.data().rating;
+          if (s < 5) {
+            const star = ` 
+           <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+            <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+            <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+            ${s / 2}`
+            $(".stars").append(star)
+          } else if (5 < s && s < 7) {
+            const star = ` 
+           <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+            <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+            ${s / 2}`
+            $(".stars").append(star)
+          } else if (7 < s && s < 9) {
+            const star = ` 
+           <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+            ${s / 2}`
+            $(".stars").append(star)
+          } else if (9 < s && s < 10) {
+            const star = ` 
+           <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            <ons-icon style="color: red" icon="fa-star"></ons-icon>
+            ${s / 2}`
+            $(".stars").append(star)
+          }
+          $("#movie_time").append(doc.data().runtime)
+
+          db.collection("movies").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              const result5 =
+                `<ons-carousel-item modifier="nodivider" id="${doc.data().title}" >
+                  <img src="${doc.data().posterURL}">
+                </ons-carousel-item>`
+
+              $("#list-movie-similar").append(result5)
+
+            });
+          });
 
         }
-        
       });
-
     });
   }
+
+
+
+
 
 
 
@@ -149,7 +222,7 @@ document.addEventListener('init', function (event) {
         console.log(displayName, email, photoUrl);
         $("#username").text(email);
         $("#displayname").text(displayName);
-        $("#photo").attr("src",photoUrl);
+        $("#photo").attr("src", photoUrl);
 
 
       }
