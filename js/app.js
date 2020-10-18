2000b1b1d36d2dd9a2e3b471b8d8d322665dcf6b
pondzapkw123 <konlawateiei@gmail.com>
2020-10-18 16:27:08 +0700

    up

--- js/app.js
+++ js/app.js
@@ -145,29 +145,90 @@ document.addEventListener('init', function (event) {
     });
 
   }
+  /*
+// movie_details.html
+function MoviesDetail(id) {
+  db.collection("movies").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      if (doc.data().title == id) {
+      
+
+        const show_pic =
+          `
+          <img src="${doc.data().posterURL}"alt="" width="60%">
+       
+        `
+        $("#show_pic").append(show_pic)
+
+        $("#movie_title").append(doc.data().title)
+        $("#movie_year").append(doc.data().year)
+        $(".movie_synopsis").append(doc.data().shortstory)
+        $(".movie_cast").append(doc.data().cast)
+        var s = doc.data().rating;
+        if (s < 5) {
+          const star = ` 
+         <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+          <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+          <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+          ${s / 2}`
+          $(".stars").append(star)
+        } else if (5 < s && s < 7) {
+          const star = ` 
+         <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+          <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+          ${s / 2}`
+          $(".stars").append(star)
+        } else if (7 < s && s < 9) {
+          const star = ` 
+         <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: gray" icon="fa-star"></ons-icon>
+          ${s / 2}`
+          $(".stars").append(star)
+        } else if (9 < s && s < 10) {
+          const star = ` 
+         <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>
+          ${s / 2}`
+          $(".stars").append(star)
+        }
+        $("#movie_time").append(doc.data().time)
+
+        db.collection("movies").get().then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            const result5 =
+              `<ons-carousel-item modifier="nodivider" id="${doc.data().title}" >
+                <img src="${doc.data().posterURL}">
+              </ons-carousel-item>`
+      
+              $("#list-movie-similar").append(result5)
+            
+          });
+        });
+
+      }
 
 
-  console.log(page.id);
-  if (page.id === 'Home') {
-    $('#back').hide();
-    page.querySelector('#johnwick').onclick = function () {
-      document.querySelector('#myNavigator').pushPage('views/detail.html');
+    });
+  });
+  document.querySelector('#myNavigator').pushPage('views/moviedetail.html');
+}
 
-    };
-    page.querySelector('#wonderwoman').onclick = function () {
-      document.querySelector('#myNavigator').pushPage('views/detail2.html');
-    };
-    page.querySelector('#toystory').onclick = function () {
-      document.querySelector('#myNavigator').pushPage('views/detail3.html');
-    };
-  } else if (page.id === 'johnwick' || page.id === 'wonderwoman' || page.id === 'toystory') {
-    $('#back').show();
-    document.querySelector('ons-back-button').onclick = function (event) {
-      document.querySelector('#myNavigator').popPage();
-    };
+function goBack() {
 
-  }
+  document.querySelector('#myNavigator').popPage()
 
+}*/
 
 });
 
